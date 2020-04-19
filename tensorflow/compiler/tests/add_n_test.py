f5bc3f44d6e91b1ff6615f30d521fc6bc6fe3ff7
Eugene Kuznetsov <eugene.kuznetsov@amd.com>
2020-04-19 21:42:44 +0000

    Fix for add_n_test

--- tensorflow/compiler/tests/add_n_test.py
+++ tensorflow/compiler/tests/add_n_test.py
@@ -18,6 +18,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+
 from tensorflow.compiler.tests import xla_test
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import errors
